d46e7d616041546ef2478b1a1a7bf6f3aaafa051
kubeboss <saikmr.knv@gmailcom>
2019-02-26 01:52:23 -0500

    modified page title

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            im kuber!
+            This is a test application made with docker!!
           </p>
           <a
             className="App-link"
